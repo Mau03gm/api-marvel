eead1a93d204adea8930f4b41cf1643e90ef55ee
Mau03gm <gatojs0109@outlook.com>
2024-12-07 15:17:10 -0600

    ...

--- src/main/java/com/MarvelDemo/demo/Resources/Hash.java
+++ src/main/java/com/MarvelDemo/demo/Resources/Hash.java
@@ -14,11 +14,9 @@ public class Hash {
 
         String toHash = ts + privateKey + publicKey;
 
-        // Generamos el hash MD5
         MessageDigest md = MessageDigest.getInstance("MD5");
         byte[] messageDigest = md.digest(toHash.getBytes());
 
-        // Convertimos el hash a formato hexadecimal
         StringBuilder hexString = new StringBuilder();
         for (byte b : messageDigest) {
             hexString.append(String.format("%02x", b));
