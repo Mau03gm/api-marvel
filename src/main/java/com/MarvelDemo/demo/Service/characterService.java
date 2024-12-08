834c0ab97ae3ce4921e7145eff79811edfa08b9c
Mau03gm <gatojs0109@outlook.com>
2024-12-08 12:26:42 -0600

    get value from properties

--- src/main/java/com/MarvelDemo/demo/Service/characterService.java
+++ src/main/java/com/MarvelDemo/demo/Service/characterService.java
@@ -1,5 +1,6 @@
 package com.MarvelDemo.demo.Service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -9,18 +10,22 @@ import java.util.logging.Logger;
 @Service
 public class characterService {
 
-    String hash="68871171626cbe72eddf213702a3e14d";
-    String privateKey="";
-    String publicKey="f3fa3ffddc4ec6463042dec1d2a8c0a4";
+    @Value("${hash}")
+    private String hash;
+    @Value("${publicKey}")
+    private String publicKey;
     String apiURL="https://gateway.marvel.com:443/v1/public/characters";
 
     public ResponseEntity<String> getAllCharacter(Integer page) {
+        Logger logger = Logger.getLogger(characterService.class.getName());
+
+        logger.info("publicKey: "+publicKey);
         page= page*10;
         RestTemplate restTemplate = new RestTemplate();
         String url = apiURL + "?ts=1&limit="+page+"&apikey=" + publicKey + "&hash=" + hash;
         ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
 
-        Logger logger = Logger.getLogger(characterService.class.getName());
+
         logger.info(response.getBody());
 
         return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
